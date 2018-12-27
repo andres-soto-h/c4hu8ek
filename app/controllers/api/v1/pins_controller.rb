fffac9be938c74f1716e87ea0b93de52564e9530
Andrés Soto <andres.soto.h@outlook.com>
2018-12-27 08:44:06 -0500

    remove sign in user

--- app/controllers/api/v1/pins_controller.rb
+++ app/controllers/api/v1/pins_controller.rb
@@ -17,7 +17,7 @@ class Api::V1::PinsController < ApplicationController
   def basic_auth
     user = User.find_by(email: request.headers['X-USER-EMAIL'])
     if user && user.api_token == request.headers['X-API-TOKEN']
-      sign_in user
+      head 200
     else
       head 401
     end
